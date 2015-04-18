34a4694ee1778595e0944a1947f8ea5d10b20dcd
Ben Van Treese <vantreeseba@gmail.com>
2015-04-18 16:27:16 -0500

    Move templates into task folder

---
+++ slush/default/task.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var gulp = require('gulp'),
+    install = require('gulp-install'),
+    conflict = require('gulp-conflict'),
+    template = require('gulp-template'),
+    rename = require('gulp-rename'),
+    _ = require('underscore.string'),
+    inquirer = require('inquirer');
+
+var prompts = require('./prompts.js');
+
+function getExcludedLicense(answers) {
+    var excludedLicense = answers.license === 'MIT' ? 'LICENSE_BSD' : 'LICENSE_MIT';
+
+    return '!' + __dirname + '/templates/' + excludedLicense;
+}
+
+function defaultTask(done) {
+    function scaffold(answers) {
+        if (!answers.moveon) {
+            return done();
+        }
+
+        answers.year = new Date().getFullYear();
+        answers.appNameSlug = _.slugify(answers.appName);
+        var files = [__dirname + '/templates/**'];
+        files.push(getExcludedLicense(answers));
+
+        gulp.src(files)
+            .pipe(template(answers))
+            .pipe(rename(function(file) {
+                if (answers.license === 'MIT') {
+                    file.basename = file.basename.replace('LICENSE_MIT', 'LICENSE');
+                } else {
+                    file.basename = file.basename.replace('LICENSE_BSD', 'LICENSE');
+                }
+                if (file.basename[0] === '_') {
+                    file.basename = '.' + file.basename.slice(1);
+                }
+            }))
+            //.pipe(conflict('./'))
+            .pipe(gulp.dest('./'))
+            .pipe(install())
+            .on('finish', function() {
+                done();
+            });
+    }
+
+    inquirer.prompt(prompts, scaffold);
+}
+
+function registerDefaultTask(){
+    gulp.task('default', defaultTask);
+}
+
+module.exports = registerDefaultTask();
